17279bfd2ee0b1506a043b42ec822035ceb3809c
JS <AioDev.JS@gmail.com>
2021-03-07 01:59:49 +0900

    BallBounce bug ifx

--- src/chap11/BallBounce.java
+++ src/chap11/BallBounce.java
@@ -16,21 +16,29 @@ import java.awt.geom.Ellipse2D;
 
 public class BallBounce extends JPanel implements ActionListener {
 
-    static final int WIDTH = 500;
-    static final int HEIGHT = 500;
+    static final int WIDTH = 1000;
+    static final int HEIGHT = 1000;
 
     private final int ballsize = 20;
     private final int halfsize = ballsize / 2;
     private Timer timer;
 
-    private float speedX = 10;
-    private float speedY = 10;
+    private float speedX;
+    private float speedY;
+
+    private long timeXA = System.currentTimeMillis();
+    private long timeXB;
+    private long timeYA = System.currentTimeMillis();
+    private long timeYB;
 
     private float ballX = WIDTH / 2;
     private float ballY = HEIGHT / 2;
 
     public BallBounce() {
-        timer = new Timer(50, this);
+        speedX = random();
+        speedY = random();
+
+        timer = new Timer(35, this);
         timer.start();
     }
 
@@ -39,32 +47,51 @@ public class BallBounce extends JPanel implements ActionListener {
         return random;
     }
 
-    public void changeSpeed() {
+    public void changeSpeedX() {
         if (ballX > (WIDTH - ballsize) || (ballX < 0)) {
             if (speedX > 0) {
                 speedX = -1 * (random());
             } else {
                 speedX = random();
-
             }
+            restrictVel();
         }
-        if ((ballY > HEIGHT - ballsize - 40) || (ballY < 0)) {
+    }
+
+    public void changeSpeedY() {
+        if ((ballY > HEIGHT - ballsize - 40) || (ballY < 20)) {
             if (speedY > 0) {
                 speedY = -1 * (random());
             } else {
                 speedY = random();
-
             }
+            restrictVel();
         }
-        restrictVel();
 
     }
 
     public void restrictVel() {
         float t = (float) (Math.abs(speedX) / Math.abs(speedY));
-        if (t > 10 || t < 0.1) {
-            changeSpeed();
+        // 공의 방향이 너무 기울어지지 않도록 조절합니다.
+        float s = (float) Math.sqrt(Math.pow(speedX, 2) + Math.pow(speedY, 2));
+        // 공의 속도가 너무 느려지지 않도록 조절 합니다.
+        if (t > 4 || t < 0.25 || s < 20) {
+            if (ballX > (WIDTH - ballsize) || (ballX < 0))
+                changeSpeedX();
+            if ((ballY > HEIGHT - ballsize - 40) || (ballY < 20))
+                changeSpeedY();
+            // 공의 방향, 속도가 적절하지 않으면 다시 랜덤을 실행합니다.
         }
+    }
+
+    public boolean timeCheck(long time1, long time2) {
+        time2 = System.currentTimeMillis();
+        if ((time2 - time1) > 400) {
+            time1 = time2;
+            return true;
+        }
+        return false;
+
     }
 
     @Override
@@ -75,11 +102,24 @@ public class BallBounce extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        changeSpeed();
-        ballX += speedX;
-        ballY += speedY;
-        System.out.println(ballX);
-        System.out.println(ballY);
+        if (ballX <= (WIDTH - ballsize) && (ballX >= 0)) {
+            ballX += speedX;
+        } else if (timeCheck(timeXA, timeXB)) {
+            changeSpeedX();
+            ballX += speedX * 2;
+        }
+
+        if (ballY <= HEIGHT - ballsize - 40 && (ballY >= 20)) {
+            ballY += speedY;
+        } else if (timeCheck(timeYA, timeYB)) {
+            changeSpeedY();
+            ballY += speedY * 2;
+        }
+
+        System.out.println("X:" + ballX);
+        System.out.println("Y:" + ballY);
+        System.out.println("SX" + speedX);
+        System.out.println("SY" + speedY);
         repaint();
 
     }
@@ -91,6 +131,7 @@ public class BallBounce extends JPanel implements ActionListener {
         frame.setDefaultCloseOperation(3);
         frame.setSize(WIDTH, HEIGHT);
         frame.setVisible(true);
+
     }
 
 }
