594cfa1499c17caa4a32c723fed69b63691f9a0f
JS <AioDev.JS@gmail.com>
2021-04-09 13:24:02 +0900

    deque

---
+++ src/chap15/DequeTest.java
@@ -0,0 +1,26 @@
+package chap15;
+
+import java.util.*;
+
+public class DequeTest {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 1; i <= 10; i++) {
+            dq.add(i); // push 가능 // addFirst //add last
+        }
+        while (dq.size() > 0) {
+            Integer val = dq.poll(); // pip
+            System.out.print(val + " ");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            dq.add(i);
+        }
+        while (dq.size() > 0) {
+            Integer val = dq.pollLast(); // pip
+            System.out.print(val + " ");
+        }
+    }
+
+}
