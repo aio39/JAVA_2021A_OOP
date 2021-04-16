c3c8408484e940b73ea5d35cd47e05ffd6f8d65c
JS <AioDev.JS@gmail.com>
2021-04-16 11:08:41 +0900

    stacktest

--- src/chap15/StackTest.java
+++ src/chap15/StackTest.java
@@ -1,11 +1,21 @@
 package chap15;
 
+import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class StackTest {
     public static void main(String[] args) {
-        test1();
+        // test1();
+        // test2();
+        for (String exp : testArr) {
+
+            checkExpression(exp);
+        }
     }
+
+    public static final String[] testArr = { "2 * ( 3 + 2 ) / ( 3 + 1 )", "2 * (( 3 + 2 ) / ( 3 + 1 )",
+            "2 * ( 3 + 2 ) / ( 3 + 1 ))", "(2 * ( 3 + 2 ) / ( 3 + 1 )" };
 
     private static void test1() {
         Stack<Character> stack = new Stack<>();
@@ -21,6 +31,41 @@ public class StackTest {
             System.out.println(nextVal);
         }
         System.out.println(stack);
+
+    }
+
+    private static void test2() {
+        String str = "APPLE/ GRAPE/ BANANA/ STRAWBERRY";
+        StringTokenizer st = new StringTokenizer(str, "/ ");
+        while (st.hasMoreTokens()) {
+            System.out.println("[" + st.nextToken() + "]");
+        }
+    }
+
+    private static void checkExpression(String exp) {
+        // System.out.println("수식을 입력해주세요");
+        // Scanner input = new Scanner(System.in);
+        // String exp = input.nextLine();
+        Stack<Character> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(exp, " ");
+        while (st.hasMoreTokens()) {
+            String nextVal = st.nextToken();
+            if (nextVal.equals("(")) {
+                stack.push(nextVal.charAt(0));
+            } else if (nextVal.equals(")")) {
+                if (stack.isEmpty()) {
+                    System.out.println("옳바르지 않은 식입니다." + exp);
+                    return;
+                }
+                stack.pop();
+
+            }
+        }
+        if (stack.isEmpty()) {
+            System.out.println("옳바른 식입니다." + exp);
+        } else {
+            System.out.println("옳바르지 않은 식입니다." + exp);
+        }
 
     }
 
